70ff150b11a9df02e60818cd5263f5596a791bae
Tyler Kotkin <Tylerkotkin@gmail.com>
2015-10-06 20:12:16 -0400

    rating and movie models created

--- movieratings/get_ratings/models.py
+++ movieratings/get_ratings/models.py
@@ -5,15 +5,15 @@ from django.db import models
 
 class Rater(models.Model):
     rater_id = models.PositiveIntegerField()
-#     movies = models.ForeignKey(Movie)
-#     rating = models.ForeignKey(Rating)
+    # movies = models.ForeignKey(Movie)
+    # rating = models.ForeignKey(Rating)
 #
-# class Movie(models.Model):
-#     movie_name = models.CharField(max_length=150)
-#     movie_id = models.PositiveIntegerField()
-#     rating = models.ForeignKey(Rating)
-#
-# class Rating(models.Model):
-#     rater = models.ForeignKey(Rater)
-#     movie = models.ForeignKey(Movie)
-#     rating = models.FloatField()
+class Movie(models.Model):
+    movie_name = models.CharField(max_length=150)
+    movie_id = models.PositiveIntegerField()
+    # rating = models.ForeignKey(Rating)
+
+class Rating(models.Model):
+    rater = models.ForeignKey(Rater)
+    movie = models.ForeignKey(Movie)
+    rating = models.FloatField()
