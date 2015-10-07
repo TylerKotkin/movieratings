753bd90ce9553cacc4851e31d721910264cee76d
Tyler Kotkin <Tylerkotkin@gmail.com>
2015-10-07 19:11:18 -0400

    data loaded

--- movieratings/get_ratings/models.py
+++ movieratings/get_ratings/models.py
@@ -4,9 +4,22 @@ from django.db import models
 
 
 class Rater(models.Model):
-    pass
-    # rater_id = models.PositiveIntegerField()
-    #
+    MALE = 'M'
+    FEMALE = 'F'
+    OTHER = 'O'
+
+
+    GENDER_CHOICES = (
+        (MALE, 'Male'),
+        (FEMALE, 'Female'),
+        (OTHER, 'Other'),
+    )
+
+    gender = models.CharField(max_length=1, choices=GENDER_CHOICES)
+    zipcode = models.CharField(max_length=5)
+    age = models.PositiveIntegerField()
+    occupation = models.CharField(max_length=40)
+
     def __str__(self):
         return 'user id: {}'.format(self.id)
 
@@ -15,14 +28,48 @@ class Movie(models.Model):
     movie_name = models.CharField(max_length=150)
     # movie_id = models.PositiveIntegerField()
 
+    def average_rating(self):
+        return self.rating_set.aggregate(models.Avg('stars'))['stars__avg']
+
     def __str__(self):
         return self.movie_name
 
 
 class Rating(models.Model):
-    rater = models.ForeignKey(Rater)
+    user = models.ForeignKey(Rater)
     movie = models.ForeignKey(Movie)
-    rating = models.FloatField()
+    stars = models.FloatField()
 
     def __str__(self):
-        return 'user {} gave {} {} stars.'.format(self.rater, self.movie, self.rating)
+        return 'user {} gave {} {} stars.'.format(self.user, self.movie, self.stars)
+
+
+def load_ml_data():
+    import csv
+    import json
+    import re
+
+    users = []
+
+    with open('ml-1m/users.dat') as f:
+        reader = csv.DictReader(f,
+                                filednames='',
+                                delimiter='')
+        for row in reader:
+            user = {
+                'fields': {
+                    'gender': row['Gender'],
+                    'age': row['Age'],
+                    'occupation': row['Occupation'],
+                    'zipcode': row['Zip-code'],
+                },
+                'model': 'get_ratings.Rater',
+                'pk': int(row['UserID']),
+            }
+
+            users.append(user)
+
+    with open('users.json', 'w') as f:
+        f.write(json.dumps(users))
+
+    # print(json.dumps(users, sort_keys=True, indent=4, separators=(',', ': ')))
