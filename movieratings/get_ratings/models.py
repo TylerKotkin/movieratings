34e35e68648dc3d908f206741a286c96884b85fc
Tyler Kotkin <Tylerkotkin@gmail.com>
2015-10-06 20:46:38 -0400

    __str__ added

--- movieratings/get_ratings/models.py
+++ movieratings/get_ratings/models.py
@@ -6,13 +6,22 @@ from django.db import models
 class Rater(models.Model):
     rater_id = models.PositiveIntegerField()
 
+    def __str__(self):
+        return 'Rater ID: {}'.format(self.rater_id)
+
 
 class Movie(models.Model):
     movie_name = models.CharField(max_length=150)
     movie_id = models.PositiveIntegerField()
+
+    def __str__(self):
+        return '{}: {}'.format(self.movie_name, self.movie_id)
 
 
 class Rating(models.Model):
     rater_id = models.ForeignKey(Rater)
     movie = models.ForeignKey(Movie)
     rating = models.FloatField()
+
+    def __str__(self):
+        return self.rating
