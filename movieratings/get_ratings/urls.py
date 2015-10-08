4f1840bfef932a02b70278e3a329513c912f1135
Tyler Kotkin <Tylerkotkin@gmail.com>
2015-10-08 15:23:13 -0400

    3 views complete and working

--- movieratings/get_ratings/urls.py
+++ movieratings/get_ratings/urls.py
@@ -3,7 +3,7 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^movies/(?P<movie_id>\d+)$', views.movie_view),
-    url(r'^raters/(?P<user_id>\d+)$', views.rater_view),
-    url(r'^top/', views.top_movies),
+    url(r'^movie/(?P<movie_id>\d+)$', views.movie_view, name='movie_view'),
+    url(r'^rater/(?P<rater_id>\d+)$', views.rater_view, name='rater_view'),
+    url(r'$', views.top_movies, name='top_movies'),
 ]
