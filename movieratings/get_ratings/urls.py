de9533338d3f0bac8c8ab97667fb65f0b2ee3450
Tyler Kotkin <Tylerkotkin@gmail.com>
2015-10-09 13:16:59 -0400

    restart

--- movieratings/get_ratings/urls.py
+++
@@ -1,9 +0,0 @@
-from django.conf.urls import url
-
-from . import views
-
-urlpatterns = [
-    url(r'^movie/(?P<movie_id>\d+)$', views.movie_view, name='movie_view'),
-    url(r'^rater/(?P<rater_id>\d+)$', views.rater_view, name='rater_view'),
-    url(r'$', views.top_movies, name='top_movies'),
-]
