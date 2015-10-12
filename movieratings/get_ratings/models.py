3260865de211ee789b0daf44dc020887a6f968c6
Tyler Kotkin <Tylerkotkin@gmail.com>
2015-10-11 22:29:51 -0400

    login created

--- movieratings/get_ratings/models.py
+++ movieratings/get_ratings/models.py
@@ -35,7 +35,6 @@ class Rater(models.Model):
         return 'user id: {}.'.format(self.id)
 
 
-
 class Rating(models.Model):
     rater = models.ForeignKey(Rater)
     movie = models.ForeignKey(Movie)
@@ -43,77 +42,3 @@ class Rating(models.Model):
 
     def __str__(self):
         return 'user {} gave {} {} stars.'.format(self.rater, self.movie, self.stars)
-
-
-def load_ml_data():
-    import csv
-    import json
-    import re
-
-    users = []
-
-    with open('ml-1m/users.dat') as f:
-        reader = csv.DictReader([line.replace('::', '\t') for line in f],
-                                fieldnames='UserID::Gender::Age::Occupation::Zip-code'.split('::'),
-                                delimiter='\t')
-        for row in reader:
-            user = {
-                'fields': {
-                    'gender': row['Gender'],
-                    'age': row['Age'],
-                    'occupation': row['Occupation'],
-                    'zipcode': row['Zip-code'],
-                },
-                'model': 'get_ratings.Rater',
-                'pk': int(row['UserID']),
-            }
-
-            users.append(user)
-
-    with open('users.json', 'w') as f:
-        f.write(json.dumps(users))
-
-    # print(json.dumps(users, sort_keys=True, indent=4, separators=(',', ': ')))
-
-    ratings = []
-
-    with open('ml-1m/ratings.dat') as f:
-        reader = csv.DictReader([line.replace('::', '\t') for line in f],
-                                fieldnames='UserID::MovieID::Rating'.split('::'),
-                                delimiter='\t')
-        for row in reader:
-            rating = {
-                'fields': {
-                    'stars': row['Rating'],
-                    'user': row['UserID'],
-                    'movie': row['MovieID']
-                },
-                'model': 'get_ratings.Rating',
-            }
-
-            ratings.append(rating)
-
-
-    with open('ratings.json', 'w') as f:
-        f.write(json.dumps(ratings))
-
-
-    movies = []
-
-    with open('ml-1m/movies.dat', encoding='windows-1252') as f:
-        reader = csv.DictReader([line.replace('::', '\t') for line in f],
-                                fieldnames='MovieID::Title::Genres'.split('::'),
-                                delimiter='\t')
-        for row in reader:
-            movie = {
-                'fields': {
-                    'title': row['Title']
-                },
-                'model': 'get_ratings.Movie',
-                'pk': row['MovieID']
-            }
-
-            movies.append(movie)
-
-    with open('movies.json', 'w') as f:
-        f.write(json.dumps(movies))
--- movieratings/movieratings/settings.py
+++ movieratings/movieratings/settings.py
@@ -35,6 +35,7 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'get_ratings',
+    'users',
 )
 
 MIDDLEWARE_CLASSES = (
