9faaac03732a5631ead066ab36cb07813d87df92
Tyler Kotkin <Tylerkotkin@gmail.com>
2015-10-11 23:16:27 -0400

    logout link added to base format

--- movieratings/movieratings/urls.py
+++ movieratings/movieratings/urls.py
@@ -22,5 +22,6 @@ urlpatterns = [
     url(r'^admin', include(admin.site.urls)),
     url(r'^register', user_views.user_register, name='user_register'),
     url(r'^login', user_views.user_login, name='user_login'),
+    url(r'^logout/$', user_views.logout_view, name='user_logout'),
     url(r'^', include('get_ratings.urls')),
 ]
