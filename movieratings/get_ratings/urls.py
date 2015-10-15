053b21f984a56e2d58977b458026e23a51d364f5
Tyler Kotkin <Tylerkotkin@gmail.com>
2015-10-14 23:04:00 -0400

    GCBV for user profile working

--- movieratings/get_ratings/urls.py
+++ movieratings/get_ratings/urls.py
@@ -3,9 +3,8 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^movie/(?P<movie_id>\d+)$', views.movie_view, name='movie_view'),
-    url(r'^rater/(?P<rater_id>\d+)$', views.rater_view, name='rater_view'),
-    url(r'^profile/(?P<rater_id>\d+)$', views.profile_view, name='profile_view'),
+    url(r'^movie/(?P<movie_id>\d+)$', views.movie_view, name='movie_detail'),
+    url(r'^profile/(?P<pk>\d+)$', views.RaterDetailView.as_view(), name='profile_view'),
     url(r'^movie/new/(?P<movie_id>\d+)$', views.new_rating, name='new_rating'),
     url(r'^movie/edit/(?P<movie_id>\d+)$', views.edit_rating, name='edit_rating'),
     url(r'^movie/unrate/(?P<movie_id>\d+)$', views.remove_rating, name='remove_rating'),
--- movieratings/get_ratings/views.py
+++ movieratings/get_ratings/views.py
@@ -16,35 +16,6 @@ from django.views import generic
 # Create your views here.
 
 
-def movie_view(request, movie_id):
-    movie = Movie.objects.get(pk=movie_id)
-    try:
-        rater_stars = request.user.rater.rating_set.filter(movie_id=movie_id)[0].stars
-    except:
-        rater_stars = ''
-    return render(request,
-                  'get_ratings/movies.html',
-                  {'movie': movie,
-                   'rater_stars': rater_stars,
-                   })
-
-
-
-
-
-# def top_movies(request):
-#     popular_movies = Movie.objects.annotate(num_ratings=Count('rating')) \
-#                                   .filter(num_ratings__gte=50)
-#
-#     movies = popular_movies.annotate(Avg('rating__stars')) \
-#                            .order_by('-rating__stars__avg')[:20]
-#
-#     most_movies = Movie.objects.annotate(num_ratings=Count('rating')).order_by('-num_ratings')[:20]
-#     return render(request,
-#                   'get_ratings/top_movies.html',
-#                   {'movies': movies,
-#                    'most_movies': most_movies})
-
 class TopIndexView(generic.ListView):
     template_name = 'get_ratings/top_movies.html'
     context_object_name = 'movies'
@@ -56,6 +27,7 @@ class TopIndexView(generic.ListView):
         return movies.annotate(Avg('rating__stars')) \
                                .order_by('-rating__stars__avg')
 
+
 class MostIndexView(generic.ListView):
     template_name = 'get_ratings/most_movies.html'
     context_object_name = 'most_movies'
@@ -65,44 +37,22 @@ class MostIndexView(generic.ListView):
         return Movie.objects.annotate(num_ratings=Count('rating')).order_by('-num_ratings')
 
 
+class RaterDetailView(generic.DetailView):
+    model = Rater
+    template = 'get_ratings/profile_detail.html'
 
 
-def rater_view(request, rater_id):
-    rater = Rater.objects.get(pk=rater_id)
-
-    # ratings = rater.rating_set.all()
-
-    # return HttpResponse(rater)
-
-    movie_ratings = []
-    for rating in rater.rating_set.all():
-        movie_ratings.append({
-            'movie': rating.movie,
-            'stars': '\u2605' * rating.stars,
-        })
-
+def movie_view(request, movie_id):
+    movie = Movie.objects.get(pk=movie_id)
+    try:
+        rater_stars = request.user.rater.rating_set.filter(movie_id=movie_id)[0].stars
+    except:
+        rater_stars = ''
     return render(request,
-                  'get_ratings/users.html',
-                  {'rater': rater,
-                   'movie_ratings': movie_ratings})
-
-
-@login_required
-def profile_view(request, rater_id):
-    rater = Rater.objects.get(pk=rater_id)
-    # ratings = rater.rating_set.all()
-    # return HttpResponse(rater)
-    movie_ratings = []
-    for rating in rater.rating_set.all():
-        movie_ratings.append({
-            'movie': rating.movie,
-            'stars': '\u2605' * rating.stars,
-        })
-    movie_ratings = rater.rating_set.all().order_by('-stars', '-posted_at')
-    return render(request,
-                  'get_ratings/profile.html',
-                  {'rater': rater,
-                   'movie_ratings': movie_ratings})
+                  'get_ratings/movie_detail.html',
+                  {'movie': movie,
+                   'rater_stars': rater_stars,
+                   })
 
 
 @login_required
@@ -116,7 +66,7 @@ def new_rating(request, movie_id):
             rating.posted_at = datetime.now()
             rating.save()
             messages.add_message(request, messages.SUCCESS, 'Your rating has been saved')
-            return redirect('movie_view', rating.movie.pk)
+            return redirect('movie_detail', rating.movie.pk)
 
     else:
         form = RatingForm()
@@ -137,4 +87,4 @@ def remove_rating(request, movie_id):
     movie = get_object_or_404(Movie, pk=movie_id)
     movie.rating_set.filter(rater=request.user.rater).delete()
     messages.add_message(request, messages.SUCCESS, 'Your rating has been deleted')
-    return redirect('movie_view', movie_id)
+    return redirect('movie_detail', movie_id)
